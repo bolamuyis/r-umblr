d9b404c377967ae0b3ed789a250e77bb4ec689dd
Bola Muyis <bolamuyis@gmail.com>
2019-01-23 14:08:10 -0500

    added the view folder

--- app.rb
+++ app.rb
@@ -1,6 +1,62 @@
 require "sinatra"
 require_relative "models"
+require 'sendgrid-ruby'
+include SendGrid
+
 
 get "/" do
-    "Hello"
+    erb :home
+end
+
+
+get "/users" do 
+  @users = User.all
+  erb :users
+end
+
+
+get "/people" do 
+    erb :people
+end
+
+
+get "/cities" do
+    erb :cities
+end
+
+
+get "/animals" do
+    erb :animals
+end
+
+
+post "/" do
+  from = SendGrid::Email.new(email: 'bolamuyis@gmail.com')
+  to = SendGrid::Email.new(email: params[:email])
+  subject = params[:name]
+  content = SendGrid::Content.new(
+    type: 'text/html',
+    value: 
+  "Damn Bola"
+  )
+  # create mail object with from, subject, to and content
+  mail = SendGrid::Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+  # sends the email
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  # display http response code
+  puts response.status_code
+
+  # display http response body
+  puts response.body
+
+  # display http response headers
+  puts response.headers
+
+  redirect "/"
+end
+
+get "/" do
+  erb :home
 end
